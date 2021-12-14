c2164e7ae45934cc5b6ebe93f09bd232b4c6ead7
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-14 14:54:11 -0600

    add: Adding AulaRepository interface to repositories package

---
+++ src/main/java/com/ibm/academia/apirest/repositories/AulaRepository.java
@@ -0,0 +1,9 @@
+package com.ibm.academia.apirest.repositories;
+
+import com.ibm.academia.apirest.models.entities.Aula;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AulaRepository extends CrudRepository<Integer, Aula> {
+  
+}
