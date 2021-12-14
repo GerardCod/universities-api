1a4e4f95a3106bbdabf0b1e7711d6bd4a4b80e22
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-14 14:48:40 -0600

    modify: Adding findEmpleadoByTipoEmpleado method to EmpleadoRepository interface

--- src/main/java/com/ibm/academia/apirest/repositories/EmpleadoRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/EmpleadoRepository.java
@@ -1,8 +1,16 @@
 package com.ibm.academia.apirest.repositories;
 
+import com.ibm.academia.apirest.enums.TipoEmpleado;
+import com.ibm.academia.apirest.models.entities.Persona;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmpleadoRepository extends PersonaRepository 
 {
+
+  @Query("select e from Empleado e where e.tipoEmpleado = ?1")
+  Iterable<Persona> findEmpleadoByTipoEmpleado(TipoEmpleado tipoEmpleado);
+
 }
