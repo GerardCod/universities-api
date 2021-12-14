1472b99a06dd6d41a2f3e4cdb9d04c55b8a043e9
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-14 15:11:01 -0600

    modify: Adding query methods for PabellonRepository

--- src/main/java/com/ibm/academia/apirest/repositories/AulaRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/AulaRepository.java
@@ -4,7 +4,9 @@ import com.ibm.academia.apirest.enums.Pizarron;
 import com.ibm.academia.apirest.models.entities.Aula;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AulaRepository extends CrudRepository<Integer, Aula> {
   
   Iterable<Aula> findAulasByPizarron(Pizarron pizarron);
@@ -12,5 +14,5 @@ public interface AulaRepository extends CrudRepository<Integer, Aula> {
   Iterable<Aula> findAulasByPabellonNombre(String nombre);
 
   Iterable<Aula> findAulasByNumeroAula(Integer numeroAula);
-  
+
 }
