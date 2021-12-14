5379e2db5c3581db432e203578f3a11f37906497
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-14 15:04:03 -0600

    modify: Adding the query methods for AulaRepository interface

--- src/main/java/com/ibm/academia/apirest/repositories/AulaRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/AulaRepository.java
@@ -1,9 +1,16 @@
 package com.ibm.academia.apirest.repositories;
 
+import com.ibm.academia.apirest.enums.Pizarron;
 import com.ibm.academia.apirest.models.entities.Aula;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface AulaRepository extends CrudRepository<Integer, Aula> {
+  
+  Iterable<Aula> findAulasByPizarron(Pizarron pizarron);
+
+  Iterable<Aula> findAulasByPabellonNombre(String nombre);
+
+  Iterable<Aula> findAulasByNumeroAula(Integer numeroAula);
   
 }
