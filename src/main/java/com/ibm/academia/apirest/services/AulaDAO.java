60caf8784b38d37ae0d1764b6b86ff6ad8e8430c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-14 18:31:46 -0600

    modify: AulaDAO interface created

---
+++ src/main/java/com/ibm/academia/apirest/services/AulaDAO.java
@@ -0,0 +1,17 @@
+package com.ibm.academia.apirest.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ibm.academia.apirest.enums.Pizarron;
+import com.ibm.academia.apirest.models.entities.Aula;
+
+public interface AulaDAO {
+  
+  Optional<List<Aula>> findAulasByPizarron(Pizarron pizarron);
+  
+  Optional<List<Aula>> findAulasByPabellonNombre(String nombre);
+  
+  Optional<List<Aula>> findAulasByNumeroAula(Integer numeroAula);
+
+}
