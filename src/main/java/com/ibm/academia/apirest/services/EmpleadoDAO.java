315396a45c085997d17c66d52d110269bc6c347e
Gera <gerardo.aguilar.calderon@gmail.com>
2021-12-17 18:58:49 -0600

    modify: Adding and implementing the updateEmployee method in EmpleadoDAO interface

--- src/main/java/com/ibm/academia/apirest/services/EmpleadoDAO.java
+++ src/main/java/com/ibm/academia/apirest/services/EmpleadoDAO.java
@@ -3,10 +3,12 @@ package com.ibm.academia.apirest.services;
 import java.util.Optional;
 
 import com.ibm.academia.apirest.enums.TipoEmpleado;
+import com.ibm.academia.apirest.models.entities.Empleado;
 import com.ibm.academia.apirest.models.entities.Persona;
 
 public interface EmpleadoDAO {
   
   Optional<Iterable<Persona>> findEmpleadoByTipoEmpleado(TipoEmpleado tipoEmpleado);
 
+  Optional<Persona> updateEmployee(Persona actual, Persona changed);
 }
--- src/main/java/com/ibm/academia/apirest/services/EmpleadoDAOImpl.java
+++ src/main/java/com/ibm/academia/apirest/services/EmpleadoDAOImpl.java
@@ -5,25 +5,40 @@ import java.util.Optional;
 import com.ibm.academia.apirest.enums.TipoEmpleado;
 import com.ibm.academia.apirest.models.entities.Persona;
 import com.ibm.academia.apirest.repositories.EmpleadoRepository;
+import com.ibm.academia.apirest.repositories.PersonaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmpleadoDAOImpl implements EmpleadoDAO {
+public class EmpleadoDAOImpl extends PersonaDAOImpl implements EmpleadoDAO {
 
   @Autowired
-  private EmpleadoRepository repository;
+  public EmpleadoDAOImpl(@Qualifier("empleadoRepository") PersonaRepository repository) {
+    super(repository);
+  }
+
 
   @Override
   public Optional<Iterable<Persona>> findEmpleadoByTipoEmpleado(TipoEmpleado tipoEmpleado) {
-    Iterable<Persona> result = repository.findEmpleadoByTipoEmpleado(tipoEmpleado);
+    Iterable<Persona> result = ((EmpleadoRepository) repository).findEmpleadoByTipoEmpleado(tipoEmpleado);
 
     if (result != null && result.iterator().hasNext()) {
       return Optional.of(result);
     }
 
     return Optional.empty();
+  }
+
+  @Override
+  public Optional<Persona> updateEmployee(Persona actual, Persona changed) {
+    actual.setNombre(changed.getNombre());
+    actual.setApellido(changed.getApellido());
+    actual.setDireccion(changed.getDireccion());
+
+    Persona result = repository.save(actual);
+    return Optional.of(result);
   }
   
 }
