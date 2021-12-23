aa311412f673ac91bb4d16024ea9bc8daef5fbfb
Gera <gerardo.aguilar.calderon@gmail.com>
2021-12-22 18:25:25 -0600

    modify: Fixing imports in PersonaController class

--- src/main/java/com/ibm/academia/apirest/controllers/PersonaController.java
+++ src/main/java/com/ibm/academia/apirest/controllers/PersonaController.java
@@ -1,19 +1,12 @@
 package com.ibm.academia.apirest.controllers;
 
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ibm.academia.apirest.exceptions.NotFoundException;
-import com.ibm.academia.apirest.models.entities.Persona;
-import com.ibm.academia.apirest.services.PersonaDAO;
 
 @RestController
 @RequestMapping("/persona")
